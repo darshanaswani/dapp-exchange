b2dbec999e9cd8bd568eb5be748e83d9ea4d4391
darshanaswani <darshanaswani63@gmail.com>
2023-01-19 19:10:06 +0530

    build commit

---
+++ build/precache-manifest.6bd02881db7d2f0b445a8ca7d4b2b339.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49698b9b5de3687d627d0ffd51330f75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9145045d4d88c0ce5e4a",
+    "url": "./static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "31af45ea2875f6cf7b7e",
+    "url": "./static/css/main.94086239.chunk.css"
+  },
+  {
+    "revision": "9145045d4d88c0ce5e4a",
+    "url": "./static/js/2.c3016fb3.chunk.js"
+  },
+  {
+    "revision": "9737e9c8afbc3220deee248435a79e61",
+    "url": "./static/js/2.c3016fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31af45ea2875f6cf7b7e",
+    "url": "./static/js/main.0c6b5b24.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.0c6b5b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0eef31aeed75a4bef26",
+    "url": "./static/js/runtime-main.13d68ef2.js"
+  }
+]);
