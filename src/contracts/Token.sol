5b97df1e0f7331d71f50c6d2ef0cce2cbe5e2b5e
darshanaswani <darshanaswani63@gmail.com>
2023-01-19 14:02:31 +0530

    initial commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
     using SafeMath for uint;
@@ -15,14 +15,21 @@ contract Token {
 
     // Events
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor() public {
         totalSupply = 1000000 * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         _transfer(msg.sender, _to, _value);
         return true;
@@ -35,14 +42,21 @@ contract Token {
         emit Transfer(_from, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
         require(_spender != address(0));
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
         allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,36 @@
-require('babel-register');
-require('babel-polyfill');
-require('dotenv').config();
-
+require("babel-register");
+require("babel-polyfill");
+require("dotenv").config(require("path").join(__dirname));
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const privateKeys = process.env.PRIVATE_KEYS || "";
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+    },
+    goerli: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(","),
+          `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`
+        );
+      },
+      gas: 4465030,
+      gasPrice: 10000000000,
+      network_id: 5,
     },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+        runs: 200,
+      },
+    },
+  },
+};
